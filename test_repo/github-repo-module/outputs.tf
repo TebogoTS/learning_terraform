e6765c0b101866759c3481e4031e1130d85d2570
Tebogo Mahlomoja <tebogo.mahlomoja@absa.africa>
2023-11-04 22:02:31 +0200

    Base update

--- test_repo/github-repo-module/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "repository_url" {
-  description = "URL of the created GitHub repository"
-  value       = github_repository.tf_lab_repo.html_url
-}
---
+++ tf_up_and_running/aws-lb.tf
@@ -0,0 +1,78 @@
+resource "aws_lb" "web_server-LB" {
+  name               = "webserver-ALB"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web_server-LB.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "webserver-sg-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "alb_asg" {
+  name     = "webserver-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "webserver-asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.web_server-LB.dns_name
+  description = "The domain name of the load balancer"
+}
